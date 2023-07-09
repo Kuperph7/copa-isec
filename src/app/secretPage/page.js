2f28216eb3cc278dc04efe0573334a125b4feea4
Tomas Kuper <kuper7ph@gmail.com>
2023-07-09 20:34:42 -0300

    first commit

---
+++ src/app/secretPage/page.js
@@ -0,0 +1,7 @@
+export default function PaginaClick() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1>El planeta de los simios será inminente</h1>
+    </main>
+  )
+}
